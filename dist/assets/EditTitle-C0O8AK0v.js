916c099322b33bb49feb82521297bd2ff860fb2d
devTee <segunade041@gmail.com>
2025-01-23 17:23:26 +0100

    build files

---
+++ dist/assets/EditTitle-C0O8AK0v.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-vqBwwr_3.js";const s=({text:e})=>r.jsx(r.Fragment,{children:r.jsx("p",{className:"border-l-4 border-primary px-2 text-stone-600",children:e})});export{s as E};
